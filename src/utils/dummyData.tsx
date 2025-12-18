323ffcbfd0e49d474f9f830a7b7fcb8bfadc88da
Elias8420 <eliaszelaya747@gmail.com>
2025-12-18 17:13:27 -0600

    add: mock data of user

--- src/utils/dummyData.tsx
+++ src/utils/dummyData.tsx
@@ -5,3 +5,27 @@ export interface User {
     email: string;
     password: string;
 };
+
+export const user: Array<User> = [
+  {
+    id: 1,
+    role: "admin",
+    name: "Juan",
+    email: "juan@example.com",
+    password: "123456"
+  },
+  {
+    id: 1,
+    role: "user",
+    name: "Juan2",
+    email: "juan2@example.com",
+    password: "12345"
+  },
+  {
+    id: 1,
+    role: "user",
+    name: "Juan3",
+    email: "jua3n@example.com",
+    password: "1234"
+  }
+];

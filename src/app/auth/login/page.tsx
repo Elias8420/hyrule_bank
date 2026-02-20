7755519136aa178f626672b834506d4c5c4f4b27
Elias8420 <eliaszelaya747@gmail.com>
2026-02-20 11:15:44 -0600

    add: styles to center login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,10 +2,10 @@ import { LoginForm } from "@/components/auth/LoginForm";
 
 export default function LoginPage() {
   return(
-    <main className="flex justify-center">
-      <div className="content-center h-screen">
-        <h1 className="">Login</h1>
-        <p className="">Acceder a tu cuenta</p>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="bg-blue-500 w-110 flex flex-col items-center p-8 gap-6">
+        <h1 className="text-2xl font-bold">Login</h1>
+        <p className="">Accede a tu cuenta</p>
         <div className="">
           <LoginForm/>
         </div>

cf6394a1592c1e729861f8940c46bd9d9c7e402a
Elias8420 <eliaszelaya747@gmail.com>
2026-02-20 10:27:48 -0600

    add: login page to main file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import LoginPage from "@/app/auth/login/page";
 
 export default function Home() {
   return (
     <div>
-      
+      <LoginPage />
     </div>
   );
 }

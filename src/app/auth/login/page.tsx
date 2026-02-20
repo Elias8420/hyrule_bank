a65196afcc3ebd0d0679725e5d88b672217edd57
Elias8420 <eliaszelaya747@gmail.com>
2026-02-20 11:46:36 -0600

    refactor: change background to form componente

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from "@/components/auth/LoginForm";
 export default function LoginPage() {
   return(
     <main className="flex justify-center items-center min-h-screen">
-      <div className="bg-blue-500 w-110 flex flex-col items-center p-8 gap-6">
+      <div className="bg-gradient-to-br from-blue-600 to-purple-700 w-110 flex flex-col items-center p-8 gap-6 rounded-xl">
         <h1 className="text-2xl font-bold">Login</h1>
         <p className="">Accede a tu cuenta</p>
         <div className="">

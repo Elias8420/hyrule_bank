d4513c171b762ce1cb5d7d68b3d6ad102bd12eb3
Elias8420 <eliaszelaya747@gmail.com>
2026-02-21 22:20:15 -0600

    refactor: reduce gap in form container and make class to form conatiner

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,8 +3,8 @@ import { LoginForm } from "@/components/auth/LoginForm";
 export default function LoginPage() {
   return(
     <main className="flex justify-center items-center min-h-screen">
-      <div className="bg-gradient-to-br from-blue-600 to-purple-700 w-110 flex flex-col items-center p-8 gap-6 rounded-xl">
-        <h1 className="text-2xl font-bold">Login</h1>
+      <div className="bg-gradient-to-br from-blue-600 to-emerald-500 w-110 flex flex-col items-center p-8 gap-4 rounded-xl">
+        <h1 className="text-2xl font-bold">Inicia Sesion</h1>
         <p className="">Accede a tu cuenta</p>
         <div className="">
           <LoginForm/>

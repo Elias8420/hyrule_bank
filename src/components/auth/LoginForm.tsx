80cf1a298da3b21628db879dccd985607b370777
Elias8420 <eliaszelaya747@gmail.com>
2026-02-27 17:33:21 -0600

    add: state when push login to see a message loading

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,18 +1,26 @@
 "use client"
 
 import { LoginAction } from "@/utils/loginAction";
+import { useState } from "react";
 
 export function LoginForm() {
+  const [loading, setLoading] = useState<boolean>(false);
   async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault(); //Evita que al enviar el formulario, recargue la pagina
 
+    
     const formData = new FormData(e.currentTarget);
     const email = String(formData.get("email") ?? "").trim(); //trim elimina los espacios de a los lados del texto
     const password = String(formData.get("password") ?? ""); // ?? "" le indica de que en caso este vacio, ponga texto vacio
-
+    
+    if(email != null && password != null)
+      setLoading(true);
+    
     const res = await LoginAction(email, password);
-  }
 
+    setLoading(false);
+  }
+  
   return(
     <form onSubmit={onSubmit} className="flex flex-col items-center w-96 gap-7">
       <div className="w-84 flex flex-col">
@@ -35,13 +43,14 @@ export function LoginForm() {
         autoComplete="current-password"
         className="border border-gray rounded-md p-2 focus:outline-none shadow-lg"
         placeholder="******"
+        disabled={loading}
         />
       </div>
 
       <button
       className="border border-white/70 p-3 rounded-xl bg-white/20 w-40 text-white font-bold hover:bg-white/40 transition-all duration-200"
       >
-        { "Iniciar Sesión" }
+        { loading ? 'Iniciando sesion...' : 'Iniciar Sesión'}
       </button>
     </form>
   );
